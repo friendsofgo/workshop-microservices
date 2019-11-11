5d445d583f7bb87f94951d1084746694bac237f4
aperezg <perezg.adrian@gmail.com>
2019-11-12 00:27:40 +0100

    improve logger middleware

--- cmd/counters-api/server/http/middleware.go
+++ cmd/counters-api/server/http/middleware.go
@@ -7,16 +7,24 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
 func (s *Server) loggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			lrw := &loggingResponseWriter{w, http.StatusOK}
+			next.ServeHTTP(lrw, r)
 
-			if currentRoute := mux.CurrentRoute(r); currentRoute != nil {
-				if routePath, err := currentRoute.GetPathTemplate(); err == nil {
-					s.logger.Printf("%s: %s\n", r.Method, routePath)
-				}
-			}
-			next.ServeHTTP(w, r)
+			path, _ := mux.CurrentRoute(r).GetPathTemplate()
+			s.logger.Printf("%d|%s: %s\n", lrw.statusCode, r.Method, path)
 		},
 	)
 }
