b0be9a083985318cd847f14d118b4371ffde411d
aperezg <perezg.adrian@gmail.com>
2019-11-19 23:06:14 +0100

    add fetch repository

--- internal/storage/mongo/counterrepository.go
+++ internal/storage/mongo/counterrepository.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	counters "github.com/friendsofgo/workshop-microservices/internal"
 )
@@ -28,4 +30,29 @@ func (c counterRepository) Save(ctx context.Context, counter *counters.Counter) 
 	}
 
 	return nil
+}
+
+func (c counterRepository) FetchAllByUser(ctx context.Context, belongsTo string) ([]counters.Counter, error) {
+	findOpts := options.Find()
+	findOpts.SetSort(bson.M{"created_at": -1})
+
+	cursor, err := c.db.Collection(c.collection).Find(ctx, bson.M{"belongs_to": belongsTo}, findOpts)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	var list []counters.Counter
+	for cursor.Next(ctx) {
+		var counter counters.Counter
+		if err := cursor.Decode(&counter); err != nil {
+			continue
+		}
+
+		list = append(list, counter)
+	}
+
+	return list, nil
 }
